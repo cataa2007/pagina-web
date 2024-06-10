7a7f648eeeb6f36184ee4dd39ca44142421e274e
Catalina Cordero <catacor2@gmail.com>
2024-06-09 21:21:22 -0300

    actualizacion pagina web

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// let precioProducto = document.getElementsByClassName
+let precioProducto = document.getElementById ("precioProducto");
+let nombreProducto = document.getElementById ("nombreProducto");
+
+precioProducto.innerText = "11.000$"
+
+// nombreProducto.style.backgroundColor = "violet"
+// nombreProducto.style.color = "white"
+
